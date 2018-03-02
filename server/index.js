4263a0b983bcbbb81faed57b449249920fdfa9c2
gloverac08 <glover.amy@gmail.com>
2018-03-02 10:54:17 -0600

    Fix server - take out that extra stuff

--- server/index.js
+++ server/index.js
@@ -3,22 +3,10 @@ var bodyParser = require('body-parser');
 var request = require('request')
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json())
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 // Due to express, when you load the page, it doesnt make a get request to '/', it simply serves up the dist folder
 app.post('/', function(req, res) {
@@ -26,7 +14,7 @@ app.post('/', function(req, res) {
 })
 
 db.sequelize.sync().then(function () {
-  http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+  app.listen(port function() {
+    console.log('listening on port ' + port + '!');
   });
 });
